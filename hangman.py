2c43b626a1390e576a6c63b5eb5d591807cc123e
ReiniervdG <reinier@vandergronden.com>
2021-04-14 11:03:17 +0200

    Incorporate regex for ascii checking, reshuffle code

--- hangman.py
+++ hangman.py
@@ -1,4 +1,6 @@
 import random
+import re
+from string import ascii_letters
 
 INPUT_PREFIX = ' > '
 
@@ -21,52 +23,56 @@ def play_game(word):
     letters = set(word_list)
 
     guessed = set()
-    guessed_list = ['_' for i in range(n)]
+    guessed_string = ['_' for i in range(n)]
 
     lives = STATIC_LIVES
 
     while lives > 0:
-        print(f'---\nGuessing word with {n} characters: {"".join(guessed_list)}')
+        print(f'---\nGuessing word with {n} characters: {"".join(guessed_string)}')
         print(f'Guessed letters: {" ".join(sorted(guessed))}')
         print(f'Lives left: {lives}')
-        print(HANGMAN_PICS[STATIC_LIVES - lives])
+        print(HANGMAN_PICS[STATIC_LIVES-lives])
 
-        letter = input(INPUT_PREFIX)
-        if len(letter) != 1:
-            if len(letter) == n:
-                if letter == word:
-                    print('You guessed the word, congratulations!')
-                    return
-                else:
-                    print('That is wrong, continue guessing letters or try another word')
-                    lives -= 1
-                    continue
-            else:
-                print(f'Please put in only one letter or {n} letters for a guess')
+        cmd = input(INPUT_PREFIX)
+        if re.match('^[a-zA-Z]$', cmd):
+            letter_guess = cmd.lower()
+
+            if letter_guess in guessed:
+                print('You already guessed this letter, please select another letter or guess the word')
                 continue
 
-        elif not letter.isalpha():
-            print('Please put in a letter a-z or A-Z')
-            continue
+            guessed.add(letter_guess)
+            if letter_guess in letters:
+                print('You got a hit!')
+                for i in [i for i, ltr in enumerate(word_list) if ltr == letter_guess]:
+                    guessed_string[i] = letter_guess
+                if '_' not in guessed_string:  # Somehow it seems better to check if the guessed and letters set are eq
+                    print(f'You win the game! The word was "{word}"')
+                    return
+            else:
+                print('That letter was not in the word, you lost a life')
+                lives -= 1
 
-        letter = letter.lower()
-
-        if letter in guessed:
-            print('You already guessed this, please select another letter')
-            continue
-
-        guessed.add(letter)
-
-        if letter.lower() in letters:
-            print('You got a hit!')
-            for i in [i for i, ltr in enumerate(word_list) if ltr == letter]:
-                guessed_list[i] = letter
-            if '_' not in guessed_list:
-                print(f'You win the game! The word was "{word}"')
+        elif re.match(f'^[a-zA-Z]{{n}}$', cmd):
+            word_guess = cmd.lower()
+            if word_guess == word:
+                print('You guessed the word, congratulations!')
                 return
+            else:
+                print('That is not the correct word, continue guessing letters or try another word')
+                lives -= 1
         else:
-            print('That letter was not in the word, you lost a life')
-            lives -= 1
+            print('Wrong input, please try again')
+            # TODO optional: Explain what was wrong about the input
+
+            # if ...
+            # print(f'Please put in only one letter or {n} letters for a guess')
+            # continue
+
+            # if not letter.isalpha():
+            # print('Please put in a letter a-z or A-Z')
+            # continue
+
     print(HANGMAN_PICS[STATIC_LIVES])
     print(f'Game over! The word was "{word}"')
 
