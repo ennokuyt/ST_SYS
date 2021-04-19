35943a13cda1d3e29b4fea266d5082fd96b84b96
ReiniervdG <reinier@vandergronden.com>
2021-04-19 14:37:20 +0200

    test plan in short

--- hangman_test.py
+++ hangman_test.py
@@ -1,4 +1,8 @@
-# Test cases
+# System imports
+
+# Custom Errors
+
+# Unit Test cases
 # 1. Empty class, guess a letter, incorporated in guessed and in guessed_string when hit
 # 2. Empty class, guess a letter, incorporated in guessed and not in guessed_string when not hit
 # 3. Word guess wrong
@@ -7,4 +11,6 @@
 # 6. Wrong length guess
 # 7. Run out of lives check
 
+# Coverage testing
 
+# Mutation testing
