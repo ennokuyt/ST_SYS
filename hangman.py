e611e832a22bea6ea679d54440e9d4eb952f09a7
ReiniervdG <reinier@vandergronden.com>
2021-04-19 16:05:05 +0200

    removed unused import, removed todos, added tracking guess and error variables to class

--- hangman.py
+++ hangman.py
@@ -1,6 +1,5 @@
 import random
 import re
-from string import ascii_letters
 
 INPUT_PREFIX = ' > '
 
@@ -16,9 +15,6 @@ HANGMAN_PICS = ['     +---+\n         |\n         |\n         |\n        ===',
                 '     +---+\n     O   |\n    /|\  |\n    / \  |\n        ===']
 
 
-# TODO class based part:
-#  raise input errors to catch in testing (?)
-#  show a message of how the game was over: out of lives or word guessed
 class Hangman:
     def __init__(self, lives=STATIC_LIVES, pics=HANGMAN_PICS, words=STATIC_WORDS):
         self.lives = lives
@@ -31,6 +27,9 @@ class Hangman:
         self.n = len(self.word_list)
         self.guessed_letters = set()
         self.guessed_string = ['_' for _ in range(self.n)]
+
+        self.word_guessed = False
+        self.last_error = 0
 
         self.play()
 
@@ -54,7 +53,7 @@ class Hangman:
                 for i in [i for i, ltr in enumerate(self.word_list) if ltr == letter_guess]:
                     self.guessed_string[i] = letter_guess
                 if '_' not in self.guessed_string:
-                    print(f'You win the game! The word was "{self.word}"')
+                    self.word_guessed = True
                     return
             else:
                 print('That letter was not in the word, you lost a life')
@@ -63,25 +62,38 @@ class Hangman:
         elif re.match(f'^[a-zA-Z]{{{self.n}}}$', cmd):
             word_guess = cmd.lower()
             if word_guess == self.word:
-                print('You guessed the word, congratulations!')
+                self.word_guessed = True
                 return
             else:
                 print('That is not the correct word, continue guessing letters or try another word')
                 self.lives -= 1
         else:
             if not re.match(f'^[a-zA-Z]+$', cmd):
-                pass
+                self.last_error = 'bad character'
+                print('Wrong input character, please provide only characters a-z or A-Z')
             elif not len(cmd) == self.n:
-                pass
+                self.last_error = 'bad length'
+                print(f'Wrong input length, please provide only a single letter guess, '
+                      f'or a word guess of {self.n} letters')
             else:
+                self.last_error = 'unknown'
                 print('Wrong input, please try again')
 
     def play(self):
         print('Welcome to Hangman - A Testers Nightmare Expansion\n'
               'In this game, you will be guessing a word related to the software testing course')
-        while self.lives > 0:
+        while self.lives > 0 and not self.word_guessed:
             self.show_info()
             cmd = input(INPUT_PREFIX)
             self.step(cmd)
+
+        if self.word_guessed:
+            print('Congratulations, you guessed the word!')
+        else:
+            print('Too bad, you ran out of lives before guessing the correct word.\n'
+                  f'The word was "{self.word}"')
         print('Thanks for playing!')
 
+
+if __name__ == '__main__':
+    h = Hangman()
