88edf5b00c5e7ffecd3c61bccf98682ac29e5616
ReiniervdG <reinier@vandergronden.com>
2021-04-19 14:18:10 +0200

    initialize test cases in comments

---
+++ hangman_test.py
@@ -0,0 +1,10 @@
+# Test cases
+# 1. Empty class, guess a letter, incorporated in guessed and in guessed_string when hit
+# 2. Empty class, guess a letter, incorporated in guessed and not in guessed_string when not hit
+# 3. Word guess wrong
+# 4. Word guess right
+# 5. Weird character -> this is probably difficult
+# 6. Wrong length guess
+# 7. Run out of lives check
+
+
