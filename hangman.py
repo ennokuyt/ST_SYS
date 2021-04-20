9d13daf93b27e67a83590bd08f3b19ffd08dd43d
ReiniervdG <reinier@vandergronden.com>
2021-04-20 10:10:37 +0200

    many many testing updates

--- hangman.py
+++ hangman.py
@@ -16,10 +16,14 @@ HANGMAN_PICS = ['     +---+\n         |\n         |\n         |\n        ===',
 
 
 class Hangman:
-    def __init__(self, lives=STATIC_LIVES, pics=HANGMAN_PICS, words=STATIC_WORDS, run=True):
+    def __init__(self, lives=STATIC_LIVES, pics=None, words=None, play=True):
+        if pics is None:
+            pics = HANGMAN_PICS
+        if words is None:
+            words = STATIC_WORDS
         self.lives = lives
-        self.pics = HANGMAN_PICS
-        self.words = STATIC_WORDS
+        self.pics = pics
+        self.words = words
 
         self.word = random.choice(self.words)
         self.word_list = list(self.word)
@@ -29,10 +33,14 @@ class Hangman:
         self.guessed_string = ['_' for _ in range(self.n)]
 
         self.word_guessed = False
-        self.last_error = 0
 
-        if run:
+        if play:
             self.play()
+
+    @staticmethod
+    def show_welcome():
+        print('Welcome to Hangman - A Testers Nightmare Expansion\n'
+              'In this game, you will be guessing a word related to the software testing course')
 
     def show_info(self):
         print(f'Guessing word with {self.n} characters: {"".join(self.guessed_string)}')
@@ -40,60 +48,91 @@ class Hangman:
         print(f'Lives left: {self.lives}')
         print(HANGMAN_PICS[-(self.lives+1)])
 
+    @staticmethod
+    def show_already_guessed():
+        print('You already guessed this letter, please select another letter or guess the word')
+
+    @staticmethod
+    def show_hit_letter():
+        print('You got a hit!')
+
+    @staticmethod
+    def show_miss_letter():
+        print('That letter was not in the word, you lost a life')
+
+    @staticmethod
+    def show_wrong_word():
+        print('That is not the correct word, continue guessing letters or try another word')
+
+    @staticmethod
+    def show_bad_char():
+        print('Wrong input character, please provide only characters a-z or A-Z')
+
+    def show_bad_length(self):
+        print(f'Wrong input length, please provide only a single letter guess, '
+              f'or a word guess of {self.n} letters')
+
+    @staticmethod
+    def show_win():
+        print('Congratulations, you guessed the word!')
+
+    def show_game_over(self):
+        print('Too bad, you ran out of lives before guessing the correct word.\n'
+              f'The word was "{self.word}"')
+
+    @staticmethod
+    def show_goodbye():
+        print('Thanks for playing!')
+
     def step(self, cmd):
         if re.match('^[a-zA-Z]$', cmd):
             letter_guess = cmd.lower()
 
             if letter_guess in self.guessed_letters:
-                print('You already guessed this letter, please select another letter or guess the word')
+                self.show_already_guessed()
                 return
 
             self.guessed_letters.add(letter_guess)
             if letter_guess in self.letters:
-                print('You got a hit!')
+                self.show_hit_letter()
                 for i in [i for i, ltr in enumerate(self.word_list) if ltr == letter_guess]:
                     self.guessed_string[i] = letter_guess
                 if '_' not in self.guessed_string:
                     self.word_guessed = True
+                    self.show_win()
                     return
             else:
-                print('That letter was not in the word, you lost a life')
+                self.show_miss_letter()
                 self.lives -= 1
 
         elif re.match(f'^[a-zA-Z]{{{self.n}}}$', cmd):
             word_guess = cmd.lower()
             if word_guess == self.word:
                 self.word_guessed = True
+                self.show_win()
                 return
             else:
-                print('That is not the correct word, continue guessing letters or try another word')
+                self.show_wrong_word()
                 self.lives -= 1
         else:
             if not re.match(f'^[a-zA-Z]+$', cmd):
-                self.last_error = 'bad character'
-                print('Wrong input character, please provide only characters a-z or A-Z')
+                self.show_bad_char()
             elif not len(cmd) == self.n:
-                self.last_error = 'bad length'
-                print(f'Wrong input length, please provide only a single letter guess, '
-                      f'or a word guess of {self.n} letters')
-            else:
-                self.last_error = 'unknown'
-                print('Wrong input, please try again')
+                self.show_bad_length()
+            # This part may be commented out because it should never be reached, else we can't reach 100 percent cvrg
+            # else:
+            #     print('Wrong input, please try again')
 
     def play(self):
-        print('Welcome to Hangman - A Testers Nightmare Expansion\n'
-              'In this game, you will be guessing a word related to the software testing course')
+        self.show_welcome()
         while self.lives > 0 and not self.word_guessed:
             self.show_info()
             cmd = input(INPUT_PREFIX)
             self.step(cmd)
 
-        if self.word_guessed:
-            print('Congratulations, you guessed the word!')
-        else:
-            print('Too bad, you ran out of lives before guessing the correct word.\n'
-                  f'The word was "{self.word}"')
-        print('Thanks for playing!')
+        if not self.word_guessed:
+            self.show_game_over()
+        self.show_goodbye()
 
 
 if __name__ == '__main__':
--- hangman_test.py
+++ hangman_test.py
@@ -1,9 +1,10 @@
 # System imports
-import io
-import sys
 import unittest
+from unittest.mock import patch, MagicMock
 
 from hangman import Hangman
+
+NUM_LIVES = 2
 
 
 # Unit Test cases
@@ -15,53 +16,91 @@ from hangman import Hangman
 # 6. Wrong length guess
 # 7. Run out of lives check
 class TestHangman(unittest.TestCase):
-    def __init__(self):
-        self.start_lives = 2
-        super().__init__()
-
     def setUp(self):
-        self.h = Hangman(lives=self.start_lives, words=['word'])
-        self.console = io.StringIO()
-        sys.stdout = self.console
+        self.h = Hangman(lives=NUM_LIVES, words=['word'], play=False)
 
+    # @unittest.mock.patch('builtins.input', side_effect=['w'])
     def test_correct_letter(self):
+        self.h.show_hit_letter = MagicMock()
         self.h.step('w')
-        assert 'hit' in self.console
-        assert self.h.lives == self.start_lives
+        self.h.show_hit_letter.assert_called_once()
+        self.assertFalse(self.h.word_guessed)
+        self.assertEqual(self.h.lives, NUM_LIVES)
 
     def test_wrong_letter(self):
+        self.h.show_miss_letter = MagicMock()
         self.h.step('a')
-        assert 'not in the word' in self.console
-        assert self.h.lives == self.start_lives - 1
+        self.h.show_miss_letter.assert_called_once()
+        self.assertFalse(self.h.word_guessed)
+        self.assertEqual(self.h.lives, NUM_LIVES - 1)
+
+    def test_already_guessed_letter(self):
+        self.h.show_miss_letter = MagicMock()
+        self.h.show_already_guessed = MagicMock()
+        self.h.step('a')
+        self.h.step('a')
+        self.h.show_miss_letter.assert_called_once()
+        self.h.show_already_guessed.assert_called_once()
+        self.assertFalse(self.h.word_guessed)
+        self.assertEqual(self.h.lives, NUM_LIVES - 1)
 
     def test_correct_word(self):
+        self.h.show_win = MagicMock()
         self.h.step('word')
-        assert self.h.word_guessed
-        assert 'Congratulations' in self.console
+        self.h.show_win.assert_called_once()
+        self.assertTrue(self.h.word_guessed)
+        self.assertEqual(self.h.lives, NUM_LIVES)
+
+    @unittest.mock.patch('builtins.input', side_effect=['w', 'o', 'r', 'd'])
+    def test_correct_word_with_letters(self, mock_input):
+        self.h.show_hit_letter = MagicMock()
+        self.h.show_win = MagicMock()
+        self.h.show_goodbye = MagicMock()
+        self.h.play()
+        self.assertEqual(self.h.show_hit_letter.call_count, 4)
+        self.h.show_win.assert_called_once()
+        self.h.show_goodbye.assert_called_once()
+        self.assertTrue(self.h.word_guessed)
+        self.assertEqual(self.h.lives, NUM_LIVES)
 
     def test_wrong_word(self):
+        self.h.show_wrong_word = MagicMock()
         self.h.step('wrod')
-        assert not self.h.word_guessed
-        assert self.h.lives == self.start_lives - 1
-        assert 'not the correct word' in self.console
+        self.h.show_wrong_word.assert_called_once()
+        self.assertFalse(self.h.word_guessed)
+        self.assertEqual(self.h.lives, NUM_LIVES - 1)
 
     def test_wrong_input_character(self):
+        self.h.show_bad_char = MagicMock()
         self.h.step('å')
-        assert self.h.lives == self.start_lives
-        assert 'character' in self.console
+        self.h.show_bad_char.assert_called_once()
+        self.assertFalse(self.h.word_guessed)
+        self.assertEqual(self.h.lives, NUM_LIVES)
 
     def test_wrong_input_length(self):
+        self.h.show_bad_length = MagicMock()
         self.h.step('wor')
-        assert 'length' in self.console
-        assert self.h.lives == self.start_lives
+        self.h.show_bad_length.assert_called_once()
+        self.assertFalse(self.h.word_guessed)
+        self.assertEqual(self.h.lives, NUM_LIVES)
 
-    def test_game_over(self):
-        self.h.step('a')
-        self.h.step('b')
-        assert self.h.lives == 0
-        assert 'Too bad' in self.console
+    @unittest.mock.patch('builtins.input', side_effect=['a', 'b'])
+    def test_game_over(self, mock_input):
+        self.h.show_miss_letter = MagicMock()
+        self.h.show_game_over = MagicMock()
+        self.h.show_goodbye = MagicMock()
+        self.h.play()
+        self.assertEqual(self.h.show_miss_letter.call_count, 2)
+        self.h.show_game_over.assert_called_once()
+        self.h.show_goodbye.assert_called_once()
+        self.assertFalse(self.h.word_guessed)
+        self.assertEqual(self.h.lives, 0)
 
 
-# Coverage testing
+if __name__ == '__main__':
+    # Run test
+    unittest.main()
 
-# Mutation testing
+    # Coverage testing
+
+    # Mutation testing
