f5f3d6f1be10e25d33883181120260588e1ef51c
ReiniervdG <reinier@vandergronden.com>
2021-04-20 10:21:58 +0200

    removed comments

--- hangman_test.py
+++ hangman_test.py
@@ -7,14 +7,6 @@ from hangman import Hangman
 NUM_LIVES = 2
 
 
-# Unit Test cases
-# 1. Empty class, guess a letter, incorporated in guessed and in guessed_string when hit
-# 2. Empty class, guess a letter, incorporated in guessed and not in guessed_string when not hit
-# 3. Word guess wrong
-# 4. Word guess right
-# 5. Weird character -> this is probably difficult
-# 6. Wrong length guess
-# 7. Run out of lives check
 class TestHangman(unittest.TestCase):
     def setUp(self):
         self.h = Hangman(lives=NUM_LIVES, words=['word'], play=False)
